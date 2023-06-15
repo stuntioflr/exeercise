c014f3a9e0844d647c70bf3a163339f1d70c7e6c
Fahad Khan <40563977+stuntioflr@users.noreply.github.com>
2023-06-15 15:45:17 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,15 +2,15 @@ import streamlit as st
 import snowflake.connector
 
 # Function to execute queries on Snowflake
-def execute_query(query):
+def execute_query(query, config):
     try:
         con = snowflake.connector.connect(
-            user='<snowflake_username>',
-            password='<snowflake_password>',
-            account='<snowflake_account_url>',
-            warehouse='<snowflake_warehouse>',
-            database='<snowflake_database>',
-            schema='<snowflake_schema>'
+            user=config['username'],
+            password=config['password'],
+            account=config['account_url'],
+            warehouse=config['warehouse'],
+            database=config['database'],
+            schema=config['schema']
         )
         cursor = con.cursor()
         cursor.execute(query)
@@ -30,21 +30,20 @@ def main():
     st.title("Snowflake Query Execution")
 
     # Button to configure Snowflake connection
-    if st.button("ep_config"):
-        with st.form_dialogue("connection_form"):
-            st.subheader("Snowflake Configuration")
-            username = st.text_input("Username")
-            password = st.text_input("Password", type="password")
-            account = st.text_input("Account URL")
-            warehouse = st.text_input("Warehouse")
-            database = st.text_input("Database")
-            schema = st.text_input("Schema")
-            submit_button = st.form_submit_button(label="Connect")
+    if st.button("Configure Snowflake"):
+        config = {}
+        config['username'] = st.text_input("Username")
+        config['password'] = st.text_input("Password", type="password")
+        config['account_url'] = st.text_input("Account URL")
+        config['warehouse'] = st.text_input("Warehouse")
+        config['database'] = st.text_input("Database")
+        config['schema'] = st.text_input("Schema")
+        submit_button = st.button("Connect")
 
-            # Execute queries if form submitted
-            if submit_button:
-                query = st.text_area("Enter your query")
-                execute_query(query)
+        # Execute queries if form submitted
+        if submit_button:
+            query = st.text_area("Enter your query")
+            execute_query(query, config)
 
 if __name__ == '__main__':
     main()
